9b87922ae9e2768c51a80bbbedb320a741508c7a
adem kalfalı <ademkalfa9@gmail.com>
2022-11-21 21:49:55 +0300

    Dashboard_update

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -6,24 +6,22 @@ import CssBaseline from "@mui/material/CssBaseline";
 import Divider from "@mui/material/Divider";
 import Drawer from "@mui/material/Drawer";
 import IconButton from "@mui/material/IconButton";
-import InboxIcon from "@mui/icons-material/MoveToInbox";
-import List from "@mui/material/List";
-import ListItem from "@mui/material/ListItem";
-import ListItemButton from "@mui/material/ListItemButton";
-import ListItemIcon from "@mui/material/ListItemIcon";
-import ListItemText from "@mui/material/ListItemText";
-import MailIcon from "@mui/icons-material/Mail";
 import MenuIcon from "@mui/icons-material/Menu";
 import Toolbar from "@mui/material/Toolbar";
 import Typography from "@mui/material/Typography";
 import MenuListItems from "../components/MenuListItems";
 import { blueGrey } from "@mui/material/colors";
+import useAuthCalls from "../hooks/useAuthCalls";
+import { useSelector } from "react-redux";
+import { Button } from "@mui/material";
 
 const drawerWidth = 200;
 
 function Dashboard(props) {
   const { window } = props;
   const [mobileOpen, setMobileOpen] = React.useState(false);
+  const { currentUser } = useSelector((state) => state.auth);
+  const { logout } = useAuthCalls();
 
   const handleDrawerToggle = () => {
     setMobileOpen(!mobileOpen);
@@ -60,9 +58,14 @@ function Dashboard(props) {
           >
             <MenuIcon />
           </IconButton>
-          <Typography variant="h6" noWrap component="div">
-            Responsive drawer
+          <Typography variant="h6" noWrap component="div" sx={{ flexGrow: 1 }}>
+            Stock App
           </Typography>
+          {currentUser && (
+            <Button color="inherit" onClick={() => logout()}>
+              Logout
+            </Button>
+          )}
         </Toolbar>
       </AppBar>
       <Box
