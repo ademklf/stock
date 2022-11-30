4dc2ccab727205a249ecbccf8ff94b68508736d4
adem kalfalı <ademkalfa9@gmail.com>
2022-11-30 14:01:31 +0300

    firms_updated

--- src/pages/Firms.jsx
+++ src/pages/Firms.jsx
@@ -1,26 +1,34 @@
 import { useEffect, useState } from "react";
+import axios from "axios";
+import { useDispatch, useSelector } from "react-redux";
 import useStockCalls from "../hooks/useStockCalls";
 import Typography from "@mui/material/Typography";
 import Box from "@mui/material/Box";
 import Button from "@mui/material/Button";
 import Grid from "@mui/material/Grid";
-import { useSelector } from "react-redux";
+import { fetchFail, fetchStart, getSuccess } from "../features/stockSlice";
 
 const Firms = () => {
-  const { getFirms } = useStockCalls();
-  const { firms } = useSelector((state) => state.stock);
-  // const { token } = useSelector((state) => state.auth);
-  // const BASE_URL = "https://13832.fullstack.clarusway.com/";
-  // const getFirms = async () => {
-  //   try {
-  //     const { data } = await axios.get(`${BASE_URL}stock/firms`, {
-  //       headers: { Authorization: `Token ${token} ` },
-  //     });
-  //     console.log(data);
-  //   } catch (error) {
-  //     console.log(error);
-  //   }
-  // };
+  const dispatch = useDispatch();
+
+  // const { getFirms } = useStockCalls();
+  // const { firms } = useSelector((state) => state.stock);
+  const { token } = useSelector((state) => state.auth);
+  const BASE_URL = "https://13832.fullstack.clarusway.com/";
+  const getFirms = async () => {
+    const url = "firms";
+    dispatch(fetchStart());
+    try {
+      const { data } = await axios.get(`${BASE_URL}stock/firms/`, {
+        headers: { Authorization: `Token ${token} ` },
+      });
+      console.log(data);
+      dispatch(getSuccess({ data, url }));
+    } catch (error) {
+      dispatch(fetchFail());
+      console.log(error);
+    }
+  };
 
   useEffect(() => {
     getFirms();
--- src/service/axiosInstance.js
+++ src/service/axiosInstance.js
@@ -1,10 +1,10 @@
 import axios from "axios";
 
-//* local storage'dan token'ı oku
+//* local storage'dan token'ı oku JSON.parse ile
 const escapedToken = JSON.parse(localStorage.getItem("persist:root"))?.token;
 const token = escapedToken && JSON.parse(escapedToken);
 
-const axiosWithToken = axios.create({
+export const axiosWithToken = axios.create({
   baseURL: "https://13832.fullstack.clarusway.com/",
   headers: { Authorization: `Token ${token}` },
 });
