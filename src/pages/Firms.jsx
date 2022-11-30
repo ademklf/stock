1e308bedcfa7399b8363f2fdf64b9a5f07e979db
adem kalfalı <ademkalfa9@gmail.com>
2022-11-30 13:05:01 +0300

    update

--- src/pages/Firms.jsx
+++ src/pages/Firms.jsx
@@ -1,36 +1,22 @@
+import axios from "axios";
 import { useEffect } from "react";
-import useStockCalls from "../hooks/useStockCalls";
-
-// import axios from "axios";
-
-// import { useDispatch, useSelector } from "react-redux";
-// import { fetchFail, fetchStart, getSuccess } from "../features/stockSlice";
+import { useSelector } from "react-redux";
 
 const Firms = () => {
-  const { getFirms, getSales } = useStockCalls();
-  // const dispatch = useDispatch();
-  // const { token } = useSelector((state) => state.auth);
-  // const BASE_URL = "https://13832.fullstack.clarusway.com/";
-  // const getFirms = async () => {
-  //   const url = "firms";
-  //   dispatch(fetchStart());
-  //   try {
-  //     const { data } = await axios.get(`${BASE_URL}stock/firms/`, {
-  //       headers: {
-  //         Authorization: `Token ${token}`,
-  //       } /*He GET işleminde Authorization içerisinde Token göndermemiz gereklidir.*/,
-  //     });
-  //     console.log(data);
-  //     dispatch(getSuccess({ data, url }));
-  //   } catch (error) {
-  //     dispatch(fetchFail());
-  //     console.log(error);
-  //   }
-  // };
+  const { token } = useSelector((state) => state.auth);
+  const BASE_URL = "https://13832.fullstack.clarusway.com/";
+  const getFirms = async () => {
+    try {
+      const { data } = await axios.get(`${BASE_URL}stock/firms`, {
+        headers: { Authorization: `Token ${token} ` },
+      });
+    } catch (error) {
+      console.log(error);
+    }
+  };
 
   useEffect(() => {
     getFirms();
-    getSales();
   }, []);
 
   return <div>Firms</div>;
