7a242cc27e42f563a9ee11f82da4015792bf0e8a
adem kalfalı <ademkalfa9@gmail.com>
2022-12-03 12:37:43 +0300

    Update

--- src/components/FirmCard.jsx
+++ src/components/FirmCard.jsx
@@ -3,22 +3,32 @@ import Card from "@mui/material/Card";
 import CardActions from "@mui/material/CardActions";
 import CardContent from "@mui/material/CardContent";
 import CardMedia from "@mui/material/CardMedia";
-import Button from "@mui/material/Button";
 import Typography from "@mui/material/Typography";
 import DeleteOutlineIcon from "@mui/icons-material/DeleteOutline";
 import EditIcon from "@mui/icons-material/Edit";
 import { btnHoverStyle } from "../styles/globalStyle";
 import useStockCalls from "../hooks/useStockCalls";
+import { CardHeader } from "@mui/material";
 
 export default function FirmCard({ firm }) {
   const { deleteFirm } = useStockCalls();
   return (
-    <Card sx={{ maxWidth: 345 }}>
+    <Card
+      elevation={10}
+      sx={{
+        p: 2,
+        width: "300px",
+        height: "400px",
+        display: "flex",
+        flexDirection: "column",
+      }}
+    >
+      <CardHeader title={firm?.name} subheader={firm?.address} />
       <CardMedia
-        component="img"
-        alt="firm-image"
-        height="140"
         image={firm?.image}
+        sx={{ p: 1, objectFit: "contain", height: "130px" }}
+        component="img"
+        alt="firm-img"
       />
       <CardContent>
         <Typography gutterBottom variant="h5" component="div">
