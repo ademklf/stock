283d3bc9d3f413e38092432b9d3d4dfebb58572d
adem kalfalı <ademkalfa9@gmail.com>
2022-12-12 21:27:08 +0300

    Brand_add

--- src/styles/globalStyle.js
+++ src/styles/globalStyle.js
@@ -17,18 +17,27 @@ export const flexCenter = {
   display: "flex",
   flexDirection: { xs: "column", sm: "row" },
   justifyContent: "center",
-  alignItem: "center",
+  alignItems: "center",
   gap: 2,
 };
-export const flexColumn = {
+
+export const flex = {
   display: "flex",
-  flexDirection: "column ",
   justifyContent: "center",
+  alignItems: "center",
   gap: 1,
 };
+
+export const flexColumn = {
+  display: "flex",
+  flexDirection: "column",
+  gap: 2,
+};
+
 export const arrowStyle = {
   display: "flex",
   justifyContent: "center",
+  alignItems: "center",
   cursor: "pointer",
   "&:hover": { color: "red" },
 };
